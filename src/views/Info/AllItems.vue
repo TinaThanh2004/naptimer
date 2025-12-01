d82daa9aaf99e262ef07b7a6362bb7f735e501df
tinanguyen <nguy4938@umn.edu>
2025-12-01 15:16:27 -0600

    add json?

---
+++ src/views/Info/AllItems.vue
@@ -0,0 +1,35 @@
+<script setup>
+import portfolioData from '@/data/portfolio.json'
+</script>
+
+<template>
+  <h1>My Portfolio</h1>
+
+  <div class="container">
+
+    <div class="preview-card" v-for="portfolioItem in portfolioData" :key="portfolioItem.id">
+      <div>{{ portfolioItem.title }}</div>
+      <img :src="portfolioItem.thumbnail" alt=""/>
+
+      <router-link :to="{ name: 'PortfolioItemDetails', params:{ portfolioId: portfolioItem.id}}">Learn More</router-link>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  gap: 10px;
+  flex-wrap: wrap;
+
+
+  .preview-card {
+    border: 1px solid black;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+}
+</style>
---
+++ src/views/Info/ItemDetail.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {useRoute} from "vue-router";
+import portfolioData from "@/data/portfolio.json";
+
+const route = useRoute()
+const itemData = portfolioData.filter(portfolioThing => portfolioThing.id === Number(route.params.portfolioId))[0]
+</script>
+
+<template>
+  <div v-if="itemData">
+    <div class="top-line">
+      <h1>{{ itemData.title }}</h1>
+      <img :src="itemData.thumbnail" alt="">
+    </div>
+  </div>
+
+  <p v-else>Sorry, this id doesn't exist</p>
+</template>
+
+<style scoped>
+.top-line {
+  display: flex;
+  justify-content: space-between;
+  border: 10px solid blue;
+}
+</style>
